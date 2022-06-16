2d3b0dbcd06b171ae605e5099d4dc20f129abf5d
martourez21 <nestorabiawuh@gmail.com>
2022-06-16 02:20:46 -0700

    task 0 first commit

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * This is a header function to return string concat
+ *
+ */
+
+int main(void)
+{
+	char *_strcat(char *dest, const char *src, size_t n)
+	{
+		size_t dest_len = strlen(dest);
+		size_t i;
+
+		for (i = 0 ; i < n && src[i] != '\0' ; i++)
+			dest[dest_len + i] = src[i];
+		dest[dest_len + i] = '\0';
+
+		return dest;
+	}
+}
+
